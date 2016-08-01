44d9c8fe5bb8d33274995dfa06dd29105335bb02
DVJex <digmvijay@gmail.com>
2016-08-01 17:34:57 +0530

    Moved dashboard data from Layout. Basics of Emp List.

--- iTanec.eCom.BusinessComponents/Properties/AssemblyInfo.cs
+++ iTanec.eCom.BusinessComponents/Properties/AssemblyInfo.cs
@@ -4,12 +4,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("UIG.Accounting.BusinessComponents")]
+[assembly: AssemblyTitle("iTanec.eCom.BusinessComponents")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("UIG.Accounting.BusinessComponents")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyProduct("iTanec.eCom.BusinessComponents")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
--- iTanec.eCom.BusinessObjects/Properties/AssemblyInfo.cs
+++ iTanec.eCom.BusinessObjects/Properties/AssemblyInfo.cs
@@ -4,12 +4,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("UIG.Accounting.BusinessObjects")]
+[assembly: AssemblyTitle("iTanec.eCom..BusinessObjects")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("UIG.Accounting.BusinessObjects")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyProduct("UiTanec.eCom..BusinessObjects")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
