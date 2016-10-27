5887703f8bdb2204a6f8f9ee690effe94ef5af3d
edpos <edwinstalinbe@gmail.com>
2016-10-27 20:30:54 +0530

    Swagger Web API Tool Implemented

---
+++ iTanec.eCom.BusinessObjects/Model/Admin/EmployeeModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iTanec.eCom.BusinessObjects.Model.Admin
+{
+    public class EmployeeModel
+    {
+        public EmployeeModel()
+        {
+            EmployeeID = 1001;
+            FirstName = "Dig";
+            LastName = "Vijay";
+            DateOfBirth = new DateTime();
+        }
+        public int EmployeeID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+    }
+
+}
--- iTanec.eCom.Pos.API/Controllers/EmployeeController.cs
+++ iTanec.eCom.Pos.API/Controllers/EmployeeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using iTanec.eCom.BusinessObjects.Model.Admin;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -7,13 +8,42 @@ using System.Web.Http;
 
 namespace iTanec.eCom.Pos.API.Controllers
 {
+    /// <summary>
+    /// API used to save the Employee Details
+    /// </summary>
     public class EmployeeController : ApiController
     {
-
-        [ActionName("GetEmplolyeeInfo")]
-        public string GetEmplolyeeInfo()
+        /// <summary>
+        /// Get Info for a single employee
+        /// </summary>
+        /// <returns></returns>
+        //[ActionName("GetEmplolyeeInfo")]
+        [HttpGet]
+        public string GetEmplolyee()
         {
             return "Edwin";
+        }
+
+        /// <summary>
+        /// Get list of employee
+        /// </summary>
+        /// <returns>list</returns>
+        //[ActionName("GetEmplolyeeList")]
+        [HttpGet]
+        public EmployeeModel ReadEmplolyee(int Id)
+        {
+            EmployeeModel EmployeeModelObj = new EmployeeModel();
+            return EmployeeModelObj;
+        }
+        /// <summary>
+        /// Get list of employee
+        /// </summary>
+        /// <returns>list</returns>
+        //[ActionName("SaveEmployee")]
+        [HttpPost]
+        public bool SaveEmployeeInformation(EmployeeModel empolyeeModel)
+        {
+            return true;
         }
     }
 }
